6ab1dca7829b47abe74d5fae524ec65b4728d56a
Tony Kovanen <tony.kovanen@gmail.com>
2020-04-11 15:07:09 +0200

    matcher gcloud function setup

---
+++ services/matcher/index.js
@@ -0,0 +1,3 @@
+const { matcher } = require("./dist/src/matcher");
+
+module.exports.matcher = matcher;
--- services/matcher/src/matcher.ts
+++ services/matcher/src/matcher.ts
@@ -1,45 +1,45 @@
 import bunyan from "bunyan";
-import Promise from "bluebird";
 import { BigQuery, BigQueryDatetime } from "@google-cloud/bigquery";
 import { PubSub } from "@google-cloud/pubsub";
 import template from "lodash.template";
 import fs from "fs";
 import config from "../config";
 const Bigtable = require("@google-cloud/bigtable");
-const BIGTABLE_INSTANCE = process.env.BIGTABLE_INSTANCE || config.bigtableInstance || "localhost:8086";
+const BIGTABLE_INSTANCE =
+  process.env.BIGTABLE_INSTANCE || config.bigtableInstance || "localhost:8086";
 const PROJECT_ID = process.env.PROJECT_ID || config.projectId;
 
 const log = bunyan.createLogger({ name: "matcher" });
-log.info("Loading SQL-query");
-const queryRaw = fs.readFileSync('sql/query.sql', 'utf8');
-log.info("Compiling SQL-query");
+console.log("Loading SQL-query");
+const queryRaw = fs.readFileSync("sql/query.sql", "utf8");
+console.log("Compiling SQL-query");
 const queryCompiled = template(queryRaw, {});
-log.info("SQL-query compiled");
+console.log("SQL-query compiled");
 
 type GeoEntry = {
-    value: string; // Polygon(...) or similar string used that can be used to insert to bigquery -- default query result fmt
-  };
-  
-  export type MatchRow = {
-    ts: BigQueryDatetime; // ISO fmt string timestamp
-    // eslint-disable-next-line @typescript-eslint/camelcase
-    user_id: string;
-    // eslint-disable-next-line @typescript-eslint/camelcase
-    push_notification_token: string;
-    segment: GeoEntry;
-    // eslint-disable-next-line @typescript-eslint/camelcase
-    infecting_user: string; // uuid string
-    // eslint-disable-next-line @typescript-eslint/camelcase
-    infecting_segment: GeoEntry;
-  };
+  value: string; // Polygon(...) or similar string used that can be used to insert to bigquery -- default query result fmt
+};
 
+export type MatchRow = {
+  ts: BigQueryDatetime; // ISO fmt string timestamp
+  // eslint-disable-next-line @typescript-eslint/camelcase
+  user_id: string;
+  // eslint-disable-next-line @typescript-eslint/camelcase
+  push_notification_token: string;
+  segment: GeoEntry;
+  // eslint-disable-next-line @typescript-eslint/camelcase
+  infecting_user: string; // uuid string
+  // eslint-disable-next-line @typescript-eslint/camelcase
+  infecting_segment: GeoEntry;
+};
 
 function generateQuery(timeSince: Date): string {
-    return queryCompiled(Object.assign(config, {
-        sickSince: timeSince.toISOString()
-    }));
+  return queryCompiled(
+    Object.assign(config, {
+      sickSince: timeSince.toISOString(),
+    })
+  );
 }
-
 
 // console.log(generateQuery(new Date()))
 
@@ -47,45 +47,61 @@ const bigtable = Bigtable({ projectId: PROJECT_ID });
 const bigquery = new BigQuery({ projectId: PROJECT_ID });
 const pubsub = new PubSub({ projectId: PROJECT_ID });
 const notificationTopic = pubsub.topic(config.pushNotificationTopic);
-const btInstance = bigtable.instance(BIGTABLE_INSTANCE)
+const btInstance = bigtable.instance(BIGTABLE_INSTANCE);
 
 const filter = [
-    {
-      column: {
-        cellLimit: 1, // Only retrieve the most recent version
-      },
+  {
+    column: {
+      cellLimit: 1, // Only retrieve the most recent version
     },
-  ];
+  },
+];
 
-const checkpointsTable = btInstance.table('checkpoints');
-const lastMatcherRun = checkpointsTable.row('last_matcher_run').get({filter});
-let runAt: null | string = null;
-lastMatcherRun.then(([row]: any) => row.data.payload)
-.then((payload: any) => new Date(payload.run_ts[0].value))
-.then((lastRunAt: Date) => {
-    log.info(`Last run at ${lastRunAt}`);
-    log.info(`Query infected since ${lastRunAt}`);
-    // Generate query taking all results since last run
-    const query = generateQuery(lastRunAt);
-    return bigquery.createQueryJob(query)
-})
-.then(([job]: any) => job.getQueryResults())
-.then(([rows]: any) => {
-    // Mark checkpoint
-    runAt = new Date().toISOString();
-    log.info(`Pushing ${rows.length} infections into push notification queue`)
-    return Promise.all(rows.map((row: MatchRow) => {
-        return notificationTopic.publish(Buffer.from(JSON.stringify(row)))
-    }, {concurrency: 100}))
-}).then((notifications: any) => {
-    log.info(`${notifications.length} notifications sent!`);
-    log.info("Saving checkpoint...");
-    return checkpointsTable.row('last_matcher_run').save({
-        payload: {run_ts: runAt}
+export async function matcher(): Promise<void> {
+  console.log("Starting matcher");
+  let runAt: null | string = null;
+
+  const checkpointsTable = btInstance.table("checkpoints");
+  const [row] = await checkpointsTable.row("last_matcher_run").get({ filter });
+  console.log("checkpoints table result", row);
+
+  const lastRunAt = row
+    ? new Date(row.data.payload.run_ts[0].value)
+    : new Date(0);
+
+  console.log(`Last run at ${lastRunAt}`);
+  console.log(`Query infected since ${lastRunAt}`);
+
+  const query = generateQuery(lastRunAt);
+  const [job] = await bigquery.createQueryJob(query);
+  const [rows] = await job.getQueryResults();
+
+  runAt = new Date().toISOString();
+  console.log(`Pushing ${rows.length} infections into push notification queue`);
+  const notifications = await Promise.all(
+    rows.map(
+      (row: MatchRow) => {
+        return notificationTopic.publish(Buffer.from(JSON.stringify(row)));
+      },
+      { concurrency: 100 }
+    )
+  );
+
+  console.log(`${notifications.length} notifications sent!`);
+  console.log("Saving checkpoint...");
+  await checkpointsTable.row("last_matcher_run").save({
+    payload: { run_ts: runAt },
+  });
+  console.log("Done!");
+}
+
+if (require.main === module) {
+  matcher()
+    .then(() => {
+      console.log("run() completed");
     })
-}).then((_: any) => {
-    log.info("Done!");
-})
-.catch((error:any) => {
-    log.error(error)
-})
+    .catch((e) => {
+      console.error("error", e);
+      process.exit(1);
+    });
+}
